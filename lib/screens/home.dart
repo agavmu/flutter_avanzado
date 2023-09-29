9eb83c69fe9c434147402ef1d3e6db741c5de816
agavmu <adriian1002@gmail.com>
2023-09-29 14:04:16 -0500

    Finalizacion del proyecto

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/socket.dart';
@@ -22,12 +23,13 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     final socket = Provider.of<Socket>(context, listen: false);
 
-    socket.socket.on('active-bands', (payload) {
-      bands = (payload as List).map((band) => Band.fromMap(band)).toList();
-      setState(() {});
-    });
-
+    socket.socket.on('active-bands', _keepActiveBands);
     super.initState();
+  }
+
+  _keepActiveBands(dynamic payload) {
+    bands = (payload as List).map((band) => Band.fromMap(band)).toList();
+    setState(() {});
   }
 
   @override
@@ -51,9 +53,16 @@ class _HomeScreenState extends State<HomeScreen> {
           )
         ],
       ),
-      body: ListView.builder(
-        itemCount: bands.length,
-        itemBuilder: (context, i) => _bandTile(bands[i]),
+      body: Column(
+        children: [
+          _showGraph(),
+          Expanded(
+            child: ListView.builder(
+              itemCount: bands.length,
+              itemBuilder: (context, i) => _bandTile(bands[i]),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
           onPressed: addNewBand, child: const Icon(Icons.add)),
@@ -75,15 +84,14 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       direction: DismissDirection.endToStart,
+      onDismissed: (_) => socket.emit('delete-band', {'id': band.id}),
       child: ListTile(
         leading: CircleAvatar(
           child: Text(band.name.substring(0, 2)),
         ),
         title: Text(band.name),
         trailing: Text('${band.votes}'),
-        onTap: () {
-          socket.emit('vote-band', {'id': band.id});
-        },
+        onTap: () => socket.emit('vote-band', {'id': band.id}),
       ),
     );
   }
@@ -93,47 +101,45 @@ class _HomeScreenState extends State<HomeScreen> {
 
     if (Platform.isAndroid) {
       return showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Text('alert'),
-            content: TextField(controller: textController),
-            actions: [
-              MaterialButton(
-                  child: const Text('add'),
-                  onPressed: () => addBandToList(textController.text))
-            ],
-          );
-        },
-      );
+          context: context,
+          builder: (_) => AlertDialog(
+                title: const Text('alert'),
+                content: TextField(controller: textController),
+                actions: [
+                  MaterialButton(
+                      child: const Text('add'),
+                      onPressed: () => {
+                            addBandToList(textController.text),
+                            Navigator.of(context).pop(true)
+                          })
+                ],
+              ));
     }
 
     showCupertinoDialog(
         context: context,
-        builder: (_) {
-          return CupertinoAlertDialog(
-            title: const Text('add new band'),
-            content: CupertinoTextField(
-              controller: textController,
-            ),
-            actions: [
-              CupertinoDialogAction(
-                isDefaultAction: true,
-                child: const Text('add'),
-                onPressed: () {
-                  addBandToList(textController.text);
-                },
+        builder: (_) => CupertinoAlertDialog(
+              title: const Text('add new band'),
+              content: CupertinoTextField(
+                controller: textController,
               ),
-              CupertinoDialogAction(
-                isDestructiveAction: true,
-                child: const Text('Dismiss'),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              )
-            ],
-          );
-        });
+              actions: [
+                CupertinoDialogAction(
+                  isDefaultAction: true,
+                  child: const Text('add'),
+                  onPressed: () {
+                    addBandToList(textController.text);
+                  },
+                ),
+                CupertinoDialogAction(
+                  isDestructiveAction: true,
+                  child: const Text('Dismiss'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            ));
   }
 
   addBandToList(String name) {
@@ -141,5 +147,46 @@ class _HomeScreenState extends State<HomeScreen> {
       final socket = Provider.of<Socket>(context, listen: false);
       socket.emit('add-band', {'name': name});
     }
+  }
+
+  _showGraph() {
+    Map<String, double> dataMap = {};
+    for (var band in bands) {
+      dataMap.putIfAbsent(band.name, () => band.votes.toDouble());
+    }
+
+    final List<Color> colorList = [
+      Colors.black38,
+      Colors.amberAccent,
+      Colors.blueAccent,
+      Colors.deepPurple,
+      Colors.indigoAccent
+    ];
+
+    return Container(
+      width: double.infinity,
+      height: 200,
+      child: PieChart(
+        dataMap: dataMap,
+        chartType: ChartType.ring,
+        baseChartColor: Colors.grey[300]!,
+        colorList: colorList,
+        legendOptions: const LegendOptions(
+          showLegendsInRow: false,
+          legendPosition: LegendPosition.right,
+          showLegends: true,
+          legendTextStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        chartValuesOptions: const ChartValuesOptions(
+          showChartValueBackground: true,
+          showChartValues: true,
+          showChartValuesInPercentage: false,
+          showChartValuesOutside: false,
+          decimalPlaces: 1,
+        ),
+      ),
+    );
   }
 }
